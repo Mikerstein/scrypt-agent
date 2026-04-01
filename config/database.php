8d677329db88aa67765528aa18013412cea6e31b
Uchedivine <uchedivine65@gmail.com>
2026-04-01 16:29:52 +0100

    fix: use DATABASE_URL directly for postgres connection

--- config/database.php
+++ config/database.php
@@ -84,19 +84,19 @@ return [
             ]) : [],
         ],
 
-       'pgsql' => [
-    'driver'   => 'pgsql',
-    'url'      => env('DATABASE_URL'),
-    'host'     => env('DB_HOST', '127.0.0.1'),
-    'port'     => env('DB_PORT', '5432'),
-    'database' => env('DB_DATABASE', 'railway'),
-    'username' => env('DB_USERNAME', 'postgres'),
-    'password' => env('DB_PASSWORD', ''),
-    'charset'  => 'utf8',
-    'prefix'   => '',
+    'pgsql' => [
+    'driver'         => 'pgsql',
+    'url'            => env('DATABASE_URL'),
+    'host'           => env('DB_HOST', '127.0.0.1'),
+    'port'           => env('DB_PORT', '5432'),
+    'database'       => env('DB_DATABASE', 'railway'),
+    'username'       => env('DB_USERNAME', 'postgres'),
+    'password'       => env('DB_PASSWORD', ''),
+    'charset'        => 'utf8',
+    'prefix'         => '',
     'prefix_indexes' => true,
-    'search_path' => 'public',
-    'sslmode'  => 'prefer',
+    'search_path'    => 'public',
+    'sslmode'        => 'prefer',
 ],
 
         'sqlsrv' => [
