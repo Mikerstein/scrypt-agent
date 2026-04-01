e93322cf627ecd97e55beda91387ac46db7041c3
Uchedivine <uchedivine65@gmail.com>
2026-04-01 19:47:10 +0100

    feat: seed admin users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,22 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::firstOrCreate(
+            ['email' => 'uchedivine65@gmail.com'],
+            ['name' => 'Admin One', 'password' => bcrypt('Sniper6.5')]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'mikazacrypto@gmail.com'],
+            ['name' => 'Admin Two', 'password' => bcrypt('Ogugua1998....')]
+        );
+
         $this->call(ScryptDataSeeder::class);
     }
 }
